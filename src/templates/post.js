a0ce5b1a36e581b4eb8e15e32eb2eda260b59c99
jbrobotics <81862056+jbrobotics@users.noreply.github.com>
2021-04-05 12:19:30 +0530

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -42,8 +42,8 @@ const PostTemplate = ({ data, location }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>{title} | Brittany Chiang</title>
-        <link rel="canonical" href="https://brittanychiang.com/pensieve" />
+        <title>{title} | Jerrin Bright</title>
+        <link rel="canonical" href="https://jbright.netlify.app" />
       </Helmet>
 
       <StyledPostContainer>
